9a756186706bfd192ba263893c79434b3dc017cf
yordi2025 <randomyordi@gmail.com>
2025-07-29 08:29:42 +0300

    banner finished

--- netflix-clone/src/pages/home/Home.js
+++ netflix-clone/src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './../../components/Header/Header';
 import Footer from './../../components/Footer/Footer';
+import Banner from '../../components/Banner/Banner';
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner/>
       <Footer/>
     </div>
   )
